a060ed154324b78bcf51befcd5a5de2d7162aa21
prof64 <80442784+prof64@users.noreply.github.com>
2022-05-13 12:27:32 -0400

    Merge RelatedNumericTag with NumericTagUtil
    NumericTagUtil has absorbed the functionality of the RelatedNumericTag module.

--- Formats/Modules/Tags/Pokerus.cs
+++ Formats/Modules/Tags/Pokerus.cs
@@ -1,6 +1,9 @@
-﻿using pkuManager.Formats.Fields;
+﻿using pkuManager.Alerts;
+using pkuManager.Formats.Fields;
 using pkuManager.Formats.Modules.Templates;
+using System;
 using System.Numerics;
+using static pkuManager.Alerts.Alert;
 using static pkuManager.Formats.PorterDirective;
 
 namespace pkuManager.Formats.Modules.Tags;
@@ -11,16 +14,18 @@ public interface Pokerus_O
     public IField<BigInteger> Pokerus_Days { get; }
 }
 
-public interface Pokerus_E : RelatedNumericTag_E
+public interface Pokerus_E : Tag
 {
-
-    // Pokérus
     [PorterDirective(ProcessingPhase.FirstPass)]
     public void ExportPokerus()
     {
         Pokerus_O pokerusObj = Data as Pokerus_O;
-        ExportRelatedNumericTag("Pokérus", new[] { "Pokérus strain", "Pokérus days" },
-            new[] { pku.Pokerus.Strain, pku.Pokerus.Days },
-            new[] { pokerusObj.Pokerus_Strain, pokerusObj.Pokerus_Days }, new BigInteger[] { 0, 0 }, false);
+        var defVals = new BigInteger[] { 0, 0 };
+        var dataFields = new[] { pokerusObj.Pokerus_Strain, pokerusObj.Pokerus_Days };
+
+        AlertType[] ats = NumericTagUtil.ExportMultiNumericTag(new[] { pku.Pokerus.Strain, pku.Pokerus.Days }, dataFields, defVals);
+        Alert a = NumericTagUtil.GetMultiNumericAlert("Pokérus", new[] { "Pokérus strain", "Pokérus days" }, ats, defVals,
+            Array.ConvertAll(dataFields, x => x as IBoundable<BigInteger>), true);
+        Warnings.Add(a);
     }
 }
--- Formats/Modules/Templates/NumericTagUtil.cs
+++ Formats/Modules/Templates/NumericTagUtil.cs
@@ -7,24 +7,59 @@ namespace pkuManager.Formats.Modules.Templates;
 
 public static class NumericTagUtil
 {
-    public static AlertType ExportNumericTag(IField<BigInteger?> pkuVal, IField<BigInteger> formatVal, BigInteger defaultVal)
+    /* ------------------------------------
+     * Common Export Functionality
+     * ------------------------------------
+    */
+    private static (BigInteger checkedVal, AlertType at)
+        CheckNumericField(IField<BigInteger?> pkuVal, IBoundable<BigInteger> bounds, BigInteger defaultVal)
     {
-        (BigInteger? max, BigInteger? min) = formatVal is IBoundable<BigInteger> boundable ?
-            (boundable.Max, boundable.Min) : (null, null);
-        (BigInteger checkedVal, AlertType at) = pkuVal.Value switch
+        //Get bounds, if any
+        (BigInteger? max, BigInteger? min) = bounds is null ? (null, null) : (bounds.Max, bounds.Min);
+
+        //round value & get alerttype
+        return pkuVal.Value switch
         {
             null => (defaultVal, AlertType.UNSPECIFIED),
             var x when x > max => (max.Value, AlertType.OVERFLOW),
             var x when x < min => (min.Value, AlertType.UNDERFLOW),
             _ => (pkuVal.Value.Value, AlertType.NONE)
         };
+    }
+
+
+    /* ------------------------------------
+     * Exporting
+     * ------------------------------------
+    */
+    public static AlertType ExportNumericTag(IField<BigInteger?> pkuVal, IField<BigInteger> formatVal, BigInteger defaultVal)
+    {
+        (BigInteger checkedVal, AlertType at) = CheckNumericField(pkuVal, formatVal as IBoundable<BigInteger>, defaultVal);
         formatVal.Value = checkedVal;
         return at;
     }
 
+    public static AlertType[] ExportMultiNumericTag(IField<BigInteger?>[] pkuVals, IField<BigInteger>[] formatVals, BigInteger[] defaultVals)
+    {
+        AlertType[] ats = new AlertType[pkuVals.Length];
+        for (int i = 0; i < pkuVals.Length; i++)
+            (formatVals[i].Value, ats[i]) = CheckNumericField(pkuVals[i], formatVals[i] as IBoundable<BigInteger>, defaultVals[i]);
+        return ats;
+    }
+
+
+    /* ------------------------------------
+     * Importing
+     * ------------------------------------
+    */
     public static void ImportNumericTag(IField<BigInteger?> pkuVal, IField<BigInteger> formatVal)
         => pkuVal.Value = formatVal.Value; //can't be invalid, all ints fit in pku fields
 
+
+    /* ------------------------------------
+     * Alerting
+     * ------------------------------------
+    */
     public static Alert GetNumericAlert(string name, AlertType at, BigInteger defaultVal, BigInteger? max, BigInteger? min) => at switch
     {
         AlertType.OVERFLOW => new(name, $"This pku's {name} is higher than the maximum. Rounding down to {max}."),
@@ -36,4 +71,31 @@ public static class NumericTagUtil
 
     public static Alert GetNumericAlert(string name, AlertType at, BigInteger defaultVal, IBoundable<BigInteger> boundable = null)
         => GetNumericAlert(name, at, defaultVal, boundable?.Max, boundable?.Min);
+
+    public static Alert GetMultiNumericAlert(string tagName, string[] subTagNames, AlertType[] ats,
+        BigInteger[] defaultVals, BigInteger?[] maxes, BigInteger?[] mins, bool ignoreUnspecified)
+    {
+        Alert a = null;
+        for (int i = 0; i < subTagNames.Length; i++)
+        {
+            if (!ignoreUnspecified || ats[i] is not AlertType.UNSPECIFIED)
+                a += GetNumericAlert(subTagNames[i], ats[i], defaultVals[i], maxes[i], mins[i]);
+        }
+        if (a is not null)
+            a.Title = tagName;
+        return a;
+    }
+
+    public static Alert GetMultiNumericAlert(string tagName, string[] subTagNames, AlertType[] ats,
+        BigInteger[] defaultVals, IBoundable<BigInteger>[] boundables, bool ignoreUnspecified)
+    {
+        BigInteger?[] maxes = new BigInteger?[boundables.Length];
+        BigInteger?[] mins = new BigInteger?[boundables.Length];
+        for (int i =0; i < boundables.Length; i++)
+        {
+            maxes[i] = boundables[i]?.Max;
+            mins[i] = boundables[i]?.Min;
+        }
+        return GetMultiNumericAlert(tagName, subTagNames, ats, defaultVals, maxes, mins, ignoreUnspecified);
+    }
 }
